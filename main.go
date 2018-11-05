861a3379bfe92e0ad03f7899e7782258f8d841de
henry <henry@icj.me>
2018-11-05 16:08:36 +0000

    basic audio integration

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"encoding/csv"
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/effects"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/vorbis"
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
@@ -392,6 +397,12 @@ func run() {
 		panic(err)
 	}
 
+	pringlePhaser := soundEffect{
+		filePath: "audio/effects/pringle-phaser.ogg",
+	}
+
+	pringlePhaser.load()
+
 	cfg := pixelgl.WindowConfig{
 		Title:  "Platformer",
 		Bounds: pixel.R(0, 0, 1024, 768),
@@ -495,6 +506,7 @@ func run() {
 		}
 
 		if win.JustPressed(pixelgl.MouseButtonLeft) {
+			go pringlePhaser.play()
 			fire.now(128, win.Bounds().Center().Add(phys.rect.Center()), win.MousePosition())
 		}
 
@@ -556,7 +568,72 @@ func run() {
 }
 
 func main() {
+
+	go loadAudio()
+
 	pixelgl.Run(run)
+}
+
+type soundEffect struct {
+	filePath string
+	format beep.Format
+	decoded beep.StreamSeekCloser
+}
+
+func(s *soundEffect) load() {
+	f1, err := os.Open(s.filePath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	s1, format, err := vorbis.Decode(f1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	s.decoded = s1
+	s.format = format
+}
+
+func (s *soundEffect) play() {
+
+	// @TODO volume sliders with effects/music control
+	// Base is 2 for human-natural, 10 would be decibels
+	effectVolume := effects.Volume{
+		Streamer: s.decoded,
+		Base: 2,
+		Volume: -3,
+	}
+
+	speaker.Play(beep.Seq(&effectVolume, beep.Callback(func() {
+		s.decoded.Seek(0)
+	})))
+}
+
+func loadAudio() {
+	f1, err := os.Open("audio/tracks/Kevin_MacLeod_Backed_Vibes_Clean.mp3")
+
+	if err != nil {
+		panic(err)
+	}
+
+	s1, format, err := mp3.Decode(f1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	playing := make(chan struct{})
+
+	speaker.Play(beep.Seq(s1, beep.Callback(func() {
+		println("close")
+		close(playing)
+	})))
+	<-playing
 }
 
 func (cl *colorlight) apply(dst pixel.ComposeTarget, center pixel.Vec, src *pixel.Sprite) {
